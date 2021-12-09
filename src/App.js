db4b3f2f415c45d5198bd042ac7e213473c2af41
Quinn Tobey Bardwell <quinntobeybardwell@quinns-air-2.dhcp.nd.edu>
2021-12-09 10:44:40 -0500

    Starting commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import NavBar from "./components/NavBar";
+
+export default function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <h1>THIS IS THE HOME PAGE</h1>
+    </div>
+  );
+}
